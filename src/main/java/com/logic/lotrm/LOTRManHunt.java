d5256e21c570b2201bd8a5b7b8f71843b251ac0d
LogicalMaximus <cornwallcaelan9@gmail.com>
2023-10-11 14:56:19 -0400

    Initial commit

---
+++ src/main/java/com/logic/lotrm/LOTRManHunt.java
@@ -0,0 +1,12 @@
+package com.logic.lotrm;
+
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = LOTRManHunt.MODID, name = LOTRManHunt.NAME, version = LOTRManHunt.VERSION, dependencies = "required-after:lotr")
+public class LOTRManHunt
+{
+    public static final String MODID = "lotrm";
+    public static final String VERSION = "0.1";
+    public static final String NAME = "LOTR Manhunt";
+}
