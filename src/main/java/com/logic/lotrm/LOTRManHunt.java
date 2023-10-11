dc798e4b374c316f77bd40660e00696efa218fea
texormiqer <147657357+texormiqer@users.noreply.github.com>
2023-10-11 22:52:00 +0300

    gradle fix
    gradle fix rara

--- src/main/java/com/logic/lotrm/LOTRManHunt.java
+++ src/main/java/com/logic/lotrm/LOTRManHunt.java
@@ -2,11 +2,32 @@ package com.logic.lotrm;
 
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = LOTRManHunt.MODID, name = LOTRManHunt.NAME, version = LOTRManHunt.VERSION, dependencies = "required-after:lotr")
+@Mod(modid = LOTRManHunt.MODID, name = LOTRManHunt.MODNAME, version = LOTRManHunt.VERSION, dependencies = "required-after:lotr")
 public class LOTRManHunt
 {
+    @Mod.Instance(LOTRManHunt.MODID)
+    public static LOTRManHunt instance;
     public static final String MODID = "lotrm";
     public static final String VERSION = "0.1";
-    public static final String NAME = "LOTR Manhunt";
+
+    public static final String MODNAME = "LOTRManhunt";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+    }
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
 }
